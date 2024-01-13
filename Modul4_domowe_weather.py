ec0f34262aa463f2487e915f837de01ee2cc9326
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-01-13 21:10:43 +0000

    weather app with user input

---
+++ Modul4_domowe_weather.py
@@ -0,0 +1,24 @@
+#stwórz słownik miasta i temperatury po pytaniu uzytkownika
+
+measurements = {}
+
+while (city := input('Podaj nazwę miasta lub "koniec" aby zakończyć: ')) != 'koniec':
+    measurements[city] = int(input(f'Podaj temperaturę dla miasta {city}: '))
+
+average = sum(measurements.values()) / len(measurements.values())
+print(f'Średnia temperatura wynosi {average:.2f}')
+
+highest_temperature = []
+lowest_temperature = []
+
+for city, temperature in measurements.items():
+    if temperature == max(measurements.values()):
+        highest_temperature.append(city)
+
+for city, temperature in measurements.items():
+    if temperature == min(measurements.values()):
+        lowest_temperature.append(city)
+
+
+print('Najwyższa temperatura', highest_temperature)
+print('Najniższa temperatura', lowest_temperature)
