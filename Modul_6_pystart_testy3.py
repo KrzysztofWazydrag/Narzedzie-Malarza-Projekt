f9364fe31947212bb9048b3720dc7d6c7165f0fe
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-03-04 23:34:18 +0000

    Another task to do with pytest

---
+++ Modul_6_pystart_testy3.py
@@ -0,0 +1,17 @@
+# Napisz funkcję, która wczyta tekst i wypisuje wszystkie wyrazy,
+# które zaczynają się na spółgłoskę i kończą na samogłoskę.
+
+def filter_words(text: str) -> set:
+    result = set()
+    for word in text.split(' '):
+        if word[0].lower() not in 'aeiouyąę' and word[-1].lower() in 'aeiouyąę':
+            result.add(word)
+
+    return result
+
+
+def test_filter_words():
+    assert filter_words('Ala ma kota') == {'ma', 'kota'}
+    assert filter_words('Ala MA KOTA') == {'MA', 'KOTA'}
+    assert filter_words('Ala Ma kotA') == {'Ma', 'kotA'}
+    assert filter_words('tygrys') == set()
