d2e2aafef73f1808d4a749b158fcd41180b21390
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-03-31 22:33:13 +0100

    updated version of reading and writing Json file

--- Modul7_booksJSON.py
+++ Modul7_booksJSON.py
@@ -1,20 +1,21 @@
 from json import load, dump
 
-new_author = "Witold Gombrowicz"
-new_title = "Ferdydurke"
-
 #otwieramy plik pierwszy raz zeby go odczytac
 with open('books.json') as file:
     books = load(file)
+    for book in books:
+        print('Tytuł', book.get('title'))
+        print('Autor:', book.get('author'))
+        print('---' * 10)
 
-for book in books:
-    print('Tytuł', book.get('title'))
-    print('Autor:', book.get('author'))
-    print('---' * 10)
 
 #otwieramy plik drugi raz zeby do niego dodac dodatkowa ksiazke i dodajemy "dump" do importu
 with open('books.json', 'w') as file:
+    new_author = input('Imię i nazwisko autora: ')
+    new_title = input('Podaj tytuł książki: ')
     books.append({
         'title': new_title,
-        'author': new_author})
+        'author': new_author
+    })
     dump(books, file)
+
