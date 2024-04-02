4a0d748c1c7fd8ac273f4b71039c8bf79e15a2be
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-04-02 22:01:58 +0100

    example of using api

---
+++ Modul7_api_example.py
@@ -0,0 +1,9 @@
+from requests import get
+
+url = 'https://api.nbp.pl/api/exchangerates/tables/A'
+codes = ['EUR', 'USD', 'GBP']
+response = get(url)
+data = response.json()
+for rate in data[0]['rates']:
+    if rate['code'] in codes:
+        print(rate['currency'], rate['mid'])
