c0b936e63f153f1fda18231658659fcde5aa8ec7
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-04-07 23:58:11 +0100

    description of task added

---
+++ Modul7_domowe_praca_z_API.py
@@ -0,0 +1,4 @@
+# Korzystając z API dostarczonego przez serwis wolnelektury pobierz wszystkie
+# dzieła konkretnego autora. Imie i nazwisko autora powinno zostać przekazane
+# jako odpowiedz na pytanie uzytkownika. Zapisz wynik do pliku o nazwie:
+# imie-nazwisko-autora.json
