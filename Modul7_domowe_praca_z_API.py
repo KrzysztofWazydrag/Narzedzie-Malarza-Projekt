95c18499e726acfc4e8c6430845b53b384480b69
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-04-08 00:21:40 +0100

    virtual library api with authors and books

--- Modul7_domowe_praca_z_API.py
+++ Modul7_domowe_praca_z_API.py
@@ -2,3 +2,20 @@
 # dzieła konkretnego autora. Imie i nazwisko autora powinno zostać przekazane
 # jako odpowiedz na pytanie uzytkownika. Zapisz wynik do pliku o nazwie:
 # imie-nazwisko-autora.json
+
+import requests
+
+def get_authors_slug(author_name: str) -> str:
+    author_request = requests.get('https://wolnelektury.pl/api/authors/')
+    for author in author_request.json():
+        if author_name == author['name']:
+            return author['slug']
+
+
+authors_slug = get_authors_slug(
+    input('Podaj imię i nazwisko autora: ')
+)
+if authors_slug is not None:
+    request = requests.get(f'https://wolnelektury.pl/api/authors/{authors_slug}/books')
+    for book in request.json():
+        print(book.get('title'))
