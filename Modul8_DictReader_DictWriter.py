d1351a90ce2fd773321a1c5bdd62edeaec1a331b
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-04-12 22:36:34 +0100

    reading and writing files using dictreader and dictwriter with examples

--- Modul8_DictReader_DictWriter.py
+++ Modul8_DictReader_DictWriter.py
@@ -7,8 +7,14 @@ import csv
 #     writer.writerow([4,'Kacper','Sieradziński'])
 
 # DictReader uzywamy jesli pierwszy wiersz pliku jest naglowkiem np typ, data, imie, nazwisko
+# i tylko wtedy kiedy kolumny mają unikatowe nazwy
+# with open ('kocur.csv', newline='') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter= ';', quotechar = '"' )
+#     for row in reader:
+#         print(row['first_name'], row['last_name'])
 
-with open ('kocur.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter= ';', quotechar = '"' )
-    for row in reader:
-        print(row['first_name'], row['last_name'])
+# Można też zapisywać do pliku csv
+with open ('orders2.csv', mode= 'w', newline='') as csvfile2:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile2, fieldnames=fieldnames)
+    writer.writerow({'first_name':'Władek', 'last_name': 'Bizneszczok'})
