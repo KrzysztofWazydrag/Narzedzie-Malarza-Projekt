44842a34debd9411f70f987cdc4087886b8c3475
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-05-08 05:34:23 +0100

    second part

--- Modul8_sql_app.py
+++ Modul8_sql_app.py
@@ -2,5 +2,16 @@ import sqlite3
 
 with sqlite3.connect('books.db') as connection:
     sql = '''CREATE TABLE books(
-    
+        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title VARCHAR(100) UNIQUE NOT NULL,
+        author VARCHAR(100)    
     )'''
+
+# utworzylismy zapytanie tworzące naszą tabelę, małe litery to wymyslone zmienne a duze pochodza z sqla
+# cursor - obiekt ktory sluzy do wykonywania zapytania
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+
+# kod powyżej tworzy bazę danych
