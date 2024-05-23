266d8e33ebd3276637c588695cdd5b43f11cd732
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-05-23 05:29:43 +0100

    zadanie domowe2 listowanie plikow w katalogu wersja kacpra

---
+++ Modul8_domowe2Kacper.py
@@ -0,0 +1,10 @@
+from os import walk
+
+with open('venv/scalone.txt', 'w') as output:
+    for path, dirs, files in walk('source'):
+        for file in files:
+            if file.endswith('txt'):
+                filename = f'{path}/{file}'
+                with open(filename) as source:
+                    output.write('\n'.join(source.readlines()))
+                    output.write('\n')
