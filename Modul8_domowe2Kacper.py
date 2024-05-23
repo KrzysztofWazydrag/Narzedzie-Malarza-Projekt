3b1af712f1a19516f802a2072ff19137a093b62c
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-05-23 05:30:38 +0100

    added description

--- Modul8_domowe2Kacper.py
+++ Modul8_domowe2Kacper.py
@@ -1,3 +1,7 @@
+# Znajdź sposób listowania plików w katalogu, a następnie napisz program, który odszuka wszystkie
+# pliki tekstowe w wybranym przez Ciebie folderze i do nowego pliku o nazwie scalone.txt
+# wstawi wynik jakim będą połączone te pliki.
+
 from os import walk
 
 with open('venv/scalone.txt', 'w') as output:
