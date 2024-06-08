afce44f2ddbafa91149607d71eef65d38656c5fe
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-06-08 05:31:02 +0100

    oop first example

---
+++ Modul9_practice1.py
@@ -0,0 +1,8 @@
+class Circle:
+    def __init__(self, radius:float):
+        self.radius = radius
+
+    def count_area(self, radius:float):
+        ...
+
+    def count_circumference(self, radius:float):
