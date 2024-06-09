c67fe87845efd0f62ced60eb31343eed1f68da19
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-06-09 22:26:36 +0100

    update

--- Modul9_practice1.py
+++ Modul9_practice1.py
@@ -1,8 +1,31 @@
+# Zaimplementuj klasę Circle, która w metodzie init powinna odebrać promień koła.
+# Klasa ta powinna posiadać dwie metody liczące pole, a także obwód koła. Pamiętaj o testach
+
+from math import pi
+
+
 class Circle:
-    def __init__(self, radius:float):
+    def __init__(self, radius: int):
         self.radius = radius
 
-    def count_area(self, radius:float):
-        ...
+# obliczamy obwód koła
+    def get_circumference(self) -> float:
+        return self.radius * 2 * pi
 
-    def count_circumference(self, radius:float):
+# obliczamy pole koła
+    def get_field(self) -> float:
+        self.radius ** 2 * pi
+
+
+def test circle_calculations():
+    circle = Circle(5)
+    field = circle.get_field()
+    circumference = circle.get_circumference()
+
+    assert 78.5 < field < 78.6
+    assert 31.4 < circumference < 31.5
+
+
+
+if __name__ == '__main__'
+
