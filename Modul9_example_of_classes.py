1dc5846fd13a12f3649daa1e6db8a9d847bfc9e5
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-06-18 00:00:53 +0100

    example of classes continue

--- Modul9_example_of_classes.py
+++ Modul9_example_of_classes.py
@@ -1,4 +1,16 @@
-class Books:
-    __init__(self):
+class Library:
+    def __init__(self):
     self.books = []
 
+# ----------->
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+# ------------>
+
+class Author:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
